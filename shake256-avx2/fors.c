c0b0f7a2658d4de9b7808fb93b91db574a32c4a6
Joost Rijneveld <joost@joostrijneveld.nl>
2018-04-24 19:21:44 +0200

    avx2: parallelize WOTS pk gen and FORS signing

---
+++ shake256-avx2/fors.c
@@ -0,0 +1,207 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "fors.h"
+#include "utils.h"
+#include "utilsx4.h"
+#include "hash.h"
+#include "hashx4.h"
+#include "hash_address.h"
+
+static void fors_gen_skx4(unsigned char *sk0,
+                          unsigned char *sk1,
+                          unsigned char *sk2,
+                          unsigned char *sk3, const unsigned char *sk_seed,
+                          uint32_t fors_leaf_addrx4[4*8])
+{
+    prf_addrx4(sk0, sk1, sk2, sk3, sk_seed, fors_leaf_addrx4);
+}
+
+static void fors_sk_to_leaf(unsigned char *leaf, const unsigned char *sk,
+                            const unsigned char *pub_seed,
+                            uint32_t fors_leaf_addr[8])
+{
+    thash(leaf, sk, 1, pub_seed, fors_leaf_addr);
+}
+
+static void fors_sk_to_leafx4(unsigned char *leaf0,
+                              unsigned char *leaf1,
+                              unsigned char *leaf2,
+                              unsigned char *leaf3,
+                              const unsigned char *sk0,
+                              const unsigned char *sk1,
+                              const unsigned char *sk2,
+                              const unsigned char *sk3,
+                              const unsigned char *pub_seed,
+                              uint32_t fors_leaf_addrx4[4*8])
+{
+    thashx4(leaf0, leaf1, leaf2, leaf3,
+            sk0, sk1, sk2, sk3, 1, pub_seed, fors_leaf_addrx4);
+}
+
+static void fors_gen_leafx4(unsigned char *leaf0,
+                            unsigned char *leaf1,
+                            unsigned char *leaf2,
+                            unsigned char *leaf3,
+                            const unsigned char *sk_seed,
+                            const unsigned char *pub_seed,
+                            uint32_t addr_idx0,
+                            uint32_t addr_idx1,
+                            uint32_t addr_idx2,
+                            uint32_t addr_idx3,
+                            const uint32_t fors_tree_addr[8])
+{
+    uint32_t fors_leaf_addrx4[4*8] = {0};
+    unsigned int j;
+
+    /* Only copy the parts that must be kept in fors_leaf_addrx4. */
+    for (j = 0; j < 4; j++) {
+        copy_keypair_addr(fors_leaf_addrx4 + j*8, fors_tree_addr);
+        set_type(fors_leaf_addrx4 + j*8, SPX_ADDR_TYPE_FORSTREE);
+    }
+
+    set_tree_index(fors_leaf_addrx4 + 0*8, addr_idx0);
+    set_tree_index(fors_leaf_addrx4 + 1*8, addr_idx1);
+    set_tree_index(fors_leaf_addrx4 + 2*8, addr_idx2);
+    set_tree_index(fors_leaf_addrx4 + 3*8, addr_idx3);
+
+    fors_gen_skx4(leaf0, leaf1, leaf2, leaf3, sk_seed, fors_leaf_addrx4);
+    fors_sk_to_leafx4(leaf0, leaf1, leaf2, leaf3,
+                      leaf0, leaf1, leaf2, leaf3, pub_seed, fors_leaf_addrx4);
+}
+
+/**
+ * Interprets m as SPX_FORS_HEIGHT-bit unsigned integers.
+ * Assumes m contains at least SPX_FORS_HEIGHT * SPX_FORS_TREES bits.
+ * Assumes indices has space for SPX_FORS_TREES integers.
+ */
+static void message_to_indices(uint32_t *indices, const unsigned char *m)
+{
+    unsigned int i, j;
+    unsigned int offset = 0;
+
+    for (i = 0; i < SPX_FORS_TREES; i++) {
+        indices[i] = 0;
+        for (j = 0; j < SPX_FORS_HEIGHT; j++) {
+            indices[i] <<= 1;
+            indices[i] ^= (m[offset >> 3] >> (offset & 0x7)) & 0x1;
+            offset++;
+        }
+    }
+}
+
+/**
+ * Signs a message m, deriving the secret key from sk_seed and the FTS address.
+ * Assumes m contains at least SPX_FORS_HEIGHT * SPX_FORS_TREES bits.
+ */
+void fors_sign(unsigned char *sig, unsigned char *pk,
+               const unsigned char *m,
+               const unsigned char *sk_seed, const unsigned char *pub_seed,
+               const uint32_t fors_addr[8])
+{
+    /* Round up to multiple of 4 to prevent out-of-bounds for x4 parallelism */
+    uint32_t indices[(SPX_FORS_TREES + 3) & ~3] = {0};
+    unsigned char roots[((SPX_FORS_TREES + 3) & ~3) * SPX_N];
+    /* Sign to a buffer, since we may not have a nice multiple of 4 and would
+       otherwise overrun the signature. */
+    unsigned char sigbufx4[4 * SPX_N * (1 + SPX_FORS_HEIGHT)];
+    uint32_t fors_tree_addrx4[4*8] = {0};
+    uint32_t fors_pk_addr[8] = {0};
+    uint32_t idx_offset[4];
+    unsigned int i, j;
+
+    for (j = 0; j < 4; j++) {
+        copy_keypair_addr(fors_tree_addrx4 + j*8, fors_addr);
+        set_type(fors_tree_addrx4 + j*8, SPX_ADDR_TYPE_FORSTREE);
+    }
+
+    copy_keypair_addr(fors_pk_addr, fors_addr);
+    set_type(fors_pk_addr, SPX_ADDR_TYPE_FORSPK);
+
+    message_to_indices(indices, m);
+
+    for (i = 0; i < ((SPX_FORS_TREES + 3) & ~0x3); i += 4) {
+        for (j = 0; j < 4; j++) {
+            if (i + j < SPX_FORS_TREES) {
+                idx_offset[j] = (i + j) * (1 << SPX_FORS_HEIGHT);
+
+                set_tree_height(fors_tree_addrx4 + j*8, 0);
+                set_tree_index(fors_tree_addrx4 + j*8,
+                               indices[i + j] + idx_offset[j]);
+            }
+        }
+
+        /* Include the secret key part that produces the selected leaf nodes. */
+        fors_gen_skx4(sigbufx4 + 0*SPX_N,
+                      sigbufx4 + 1*SPX_N,
+                      sigbufx4 + 2*SPX_N,
+                      sigbufx4 + 3*SPX_N,
+                      sk_seed, fors_tree_addrx4);
+
+        treehashx4(roots + i*SPX_N, sigbufx4 + 4*SPX_N, sk_seed, pub_seed,
+                   &indices[i], idx_offset, SPX_FORS_HEIGHT, fors_gen_leafx4,
+                   fors_tree_addrx4);
+
+        for (j = 0; j < 4; j++) {
+            if (i + j < SPX_FORS_TREES) {
+                memcpy(sig, sigbufx4 + j*SPX_N, SPX_N);
+                memcpy(sig + SPX_N,
+                       sigbufx4 + 4*SPX_N + j*SPX_N*SPX_FORS_HEIGHT,
+                       SPX_N*SPX_FORS_HEIGHT);
+                sig += SPX_N * (1 + SPX_FORS_HEIGHT);
+            }
+        }
+    }
+
+    /* Hash horizontally across all tree roots to derive the public key. */
+    thash(pk, roots, SPX_FORS_TREES, pub_seed, fors_pk_addr);
+}
+
+/**
+ * Derives the FORS public key from a signature.
+ * This can be used for verification by comparing to a known public key, or to
+ * subsequently verify a signature on the derived public key. The latter is the
+ * typical use-case when used as an FTS below an OTS in a hypertree.
+ * Assumes m contains at least SPX_FORS_HEIGHT * SPX_FORS_TREES bits.
+ */
+void fors_pk_from_sig(unsigned char *pk,
+                      const unsigned char *sig, const unsigned char *m,
+                      const unsigned char *pub_seed,
+                      const uint32_t fors_addr[8])
+{
+    uint32_t indices[SPX_FORS_TREES];
+    unsigned char roots[SPX_FORS_TREES * SPX_N];
+    unsigned char leaf[SPX_N];
+    uint32_t fors_tree_addr[8] = {0};
+    uint32_t fors_pk_addr[8] = {0};
+    uint32_t idx_offset;
+    unsigned int i;
+
+    copy_keypair_addr(fors_tree_addr, fors_addr);
+    copy_keypair_addr(fors_pk_addr, fors_addr);
+
+    set_type(fors_tree_addr, SPX_ADDR_TYPE_FORSTREE);
+    set_type(fors_pk_addr, SPX_ADDR_TYPE_FORSPK);
+
+    message_to_indices(indices, m);
+
+    for (i = 0; i < SPX_FORS_TREES; i++) {
+        idx_offset = i * (1 << SPX_FORS_HEIGHT);
+
+        set_tree_height(fors_tree_addr, 0);
+        set_tree_index(fors_tree_addr, indices[i] + idx_offset);
+
+        /* Derive the leaf from the included secret key part. */
+        fors_sk_to_leaf(leaf, sig, pub_seed, fors_tree_addr);
+        sig += SPX_N;
+
+        /* Derive the corresponding root node of this tree. */
+        compute_root(roots + i*SPX_N, leaf, indices[i], idx_offset,
+                     sig, SPX_FORS_HEIGHT, pub_seed, fors_tree_addr);
+        sig += SPX_N * SPX_FORS_HEIGHT;
+    }
+
+    /* Hash horizontally across all tree roots to derive the public key. */
+    thash(pk, roots, SPX_FORS_TREES, pub_seed, fors_pk_addr);
+}
---
+++ shake256-avx2/wots.c
@@ -0,0 +1,230 @@
+#include <stdint.h>
+#include <string.h>
+
+#include "utils.h"
+#include "hash.h"
+#include "hashx4.h"
+#include "wots.h"
+#include "hash_address.h"
+#include "params.h"
+
+// TODO clarify address expectations, and make them more uniform.
+// TODO i.e. do we expect types to be set already?
+// TODO and do we expect modifications or copies?
+
+/**
+ * Computes the starting value for a chain, i.e. the secret key.
+ * Expects the address to be complete up to the chain address.
+ */
+static void wots_gen_sk(unsigned char *sk, const unsigned char *sk_seed,
+                        uint32_t wots_addr[8])
+{
+    /* Make sure that the hash address is actually zeroed. */
+    set_hash_addr(wots_addr, 0);
+
+    /* Generate sk element. */
+    prf_addr(sk, sk_seed, wots_addr);
+}
+
+/**
+ * 4-way parallel version of wots_gen_sk; expects 4x as much space in sk
+ */
+static void wots_gen_skx4(unsigned char *skx4, const unsigned char *sk_seed,
+                          uint32_t wots_addrx4[4*8])
+{
+    unsigned int j;
+
+    /* Make sure that the hash address is actually zeroed. */
+    for (j = 0; j < 4; j++) {
+        set_hash_addr(wots_addrx4 + j*8, 0);
+    }
+
+    /* Generate sk element. */
+    prf_addrx4(skx4 + 0*SPX_N,
+               skx4 + 1*SPX_N,
+               skx4 + 2*SPX_N,
+               skx4 + 3*SPX_N,
+               sk_seed, wots_addrx4);
+}
+
+/**
+ * Computes the chaining function.
+ * out and in have to be n-byte arrays.
+ *
+ * Interprets in as start-th value of the chain.
+ * addr has to contain the address of the chain.
+ */
+static void gen_chain(unsigned char *out, const unsigned char *in,
+                      unsigned int start, unsigned int steps,
+                      const unsigned char *pub_seed, uint32_t addr[8])
+{
+    uint32_t i;
+
+    /* Initialize out with the value at position 'start'. */
+    memcpy(out, in, SPX_N);
+
+    /* Iterate 'steps' calls to the hash function. */
+    for (i = start; i < (start+steps) && i < SPX_WOTS_W; i++) {
+        set_hash_addr(addr, i);
+        thash(out, out, 1, pub_seed, addr);
+    }
+}
+
+/**
+ * 4-way parallel version of gen_chain; expects 4x as much space in out, and
+ * 4x as much space in inx4. Assumes start and step identical across chains.
+ */
+static void gen_chainx4(unsigned char *outx4, const unsigned char *inx4,
+                        unsigned int start, unsigned int steps,
+                        const unsigned char *pub_seed, uint32_t addrx4[4*8])
+{
+    uint32_t i;
+    unsigned int j;
+
+    /* Initialize outx4 with the value at position 'start'. */
+    memcpy(outx4, inx4, 4*SPX_N);
+
+    /* Iterate 'steps' calls to the hash function. */
+    for (i = start; i < (start+steps) && i < SPX_WOTS_W; i++) {
+        for (j = 0; j < 4; j++) {
+            set_hash_addr(addrx4 + j*8, i);
+        }
+        thashx4(outx4 + 0*SPX_N,
+                outx4 + 1*SPX_N,
+                outx4 + 2*SPX_N,
+                outx4 + 3*SPX_N,
+                outx4 + 0*SPX_N,
+                outx4 + 1*SPX_N,
+                outx4 + 2*SPX_N,
+                outx4 + 3*SPX_N, 1, pub_seed, addrx4);
+    }
+}
+
+/**
+ * base_w algorithm as described in draft.
+ * Interprets an array of bytes as integers in base w.
+ * This only works when log_w is a divisor of 8.
+ */
+static void base_w(int *output, const int out_len, const unsigned char *input)
+{
+    int in = 0;
+    int out = 0;
+    unsigned char total;
+    int bits = 0;
+    int consumed;
+
+    for (consumed = 0; consumed < out_len; consumed++) {
+        if (bits == 0) {
+            total = input[in];
+            in++;
+            bits += 8;
+        }
+        bits -= SPX_WOTS_LOGW;
+        output[out] = (total >> bits) & (SPX_WOTS_W - 1);
+        out++;
+    }
+}
+
+/* Computes the WOTS+ checksum over a message (in base_w). */
+static void wots_checksum(int *csum_base_w, const int *msg_base_w)
+{
+    int csum = 0;
+    unsigned char csum_bytes[(SPX_WOTS_LEN2 * SPX_WOTS_LOGW + 7) / 8];
+    unsigned int i;
+
+    /* Compute checksum. */
+    for (i = 0; i < SPX_WOTS_LEN1; i++) {
+        csum += SPX_WOTS_W - 1 - msg_base_w[i];
+    }
+
+    /* Convert checksum to base_w. */
+    /* Make sure expected empty zero bits are the least significant bits. */
+    csum = csum << (8 - ((SPX_WOTS_LEN2 * SPX_WOTS_LOGW) % 8));
+    ull_to_bytes(csum_bytes, sizeof(csum_bytes), csum);
+    base_w(csum_base_w, SPX_WOTS_LEN2, csum_bytes);
+}
+
+/* Takes a message and derives the matching chain lengths. */
+static void chain_lengths(int *lengths, const unsigned char *msg)
+{
+    base_w(lengths, SPX_WOTS_LEN1, msg);
+    wots_checksum(lengths + SPX_WOTS_LEN1, lengths);
+}
+
+/**
+ * WOTS key generation. Takes a 32 byte sk_seed, expands it to WOTS private key
+ * elements and computes the corresponding public key.
+ * It requires the seed pub_seed (used to generate bitmasks and hash keys)
+ * and the address of this WOTS key pair.
+ *
+ * Writes the computed public key to 'pk'.
+ */
+void wots_gen_pk(unsigned char *pk, const unsigned char *sk_seed,
+                 const unsigned char *pub_seed, uint32_t addr[8])
+{
+    uint32_t i;
+    unsigned int j;
+
+    uint32_t addrx4[4 * 8];
+    unsigned char pkbuf[4 * SPX_N];
+
+    for (j = 0; j < 4; j++) {
+        memcpy(addrx4 + j*8, addr, sizeof(uint32_t) * 8);
+    }
+
+    /* The last iteration typically does not have complete set of 4 chains,
+       but because we use pkbuf, this is not an issue -- we still do as many
+       in parallel as possible. */
+    for (i = 0; i < ((SPX_WOTS_LEN + 3) & ~0x3); i += 4) {
+        for (j = 0; j < 4; j++) {
+            set_chain_addr(addrx4 + j*8, i + j);
+        }
+        wots_gen_skx4(pkbuf, sk_seed, addrx4);
+        gen_chainx4(pkbuf, pkbuf, 0, SPX_WOTS_W - 1, pub_seed, addrx4);
+        for (j = 0; j < 4; j++) {
+            if (i + j < SPX_WOTS_LEN) {
+                memcpy(pk + (i + j)*SPX_N, pkbuf + j*SPX_N, SPX_N);
+            }
+        }
+    }
+}
+
+/**
+ * Takes a n-byte message and the 32-byte sk_see to compute a signature 'sig'.
+ */
+void wots_sign(unsigned char *sig, const unsigned char *msg,
+               const unsigned char *sk_seed, const unsigned char *pub_seed,
+               uint32_t addr[8])
+{
+    int lengths[SPX_WOTS_LEN];
+    uint32_t i;
+
+    chain_lengths(lengths, msg);
+
+    for (i = 0; i < SPX_WOTS_LEN; i++) {
+        set_chain_addr(addr, i);
+        wots_gen_sk(sig + i*SPX_N, sk_seed, addr);
+        gen_chain(sig + i*SPX_N, sig + i*SPX_N, 0, lengths[i], pub_seed, addr);
+    }
+}
+
+/**
+ * Takes a WOTS signature and an n-byte message, computes a WOTS public key.
+ *
+ * Writes the computed public key to 'pk'.
+ */
+void wots_pk_from_sig(unsigned char *pk,
+                      const unsigned char *sig, const unsigned char *msg,
+                      const unsigned char *pub_seed, uint32_t addr[8])
+{
+    int lengths[SPX_WOTS_LEN];
+    uint32_t i;
+
+    chain_lengths(lengths, msg);
+
+    for (i = 0; i < SPX_WOTS_LEN; i++) {
+        set_chain_addr(addr, i);
+        gen_chain(pk + i*SPX_N, sig + i*SPX_N,
+                  lengths[i], SPX_WOTS_W - 1 - lengths[i], pub_seed, addr);
+    }
+}
