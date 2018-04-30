9beb27613e1530b08b1be4cbc5b8c658d4010776
Joost Rijneveld <joost@joostrijneveld.nl>
2018-04-30 17:41:36 +0200

    Add a unit test for 4x parallel thash

---
+++ shake256-avx2/test/thashx4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "../hashx4.h"
+#include "../hash.h"
+#include "../randombytes.h"
+#include "../params.h"
+
+int main()
+{
+    /* Make stdout buffer more responsive. */
+    setbuf(stdout, NULL);
+
+    unsigned char input[4*SPX_N];
+    unsigned char seed[SPX_N];
+    unsigned char output[4*SPX_N];
+    unsigned char out4[4*SPX_N];
+    uint32_t addr[4*8] = {0};
+    unsigned int j;
+
+    randombytes(seed, SPX_N);
+    randombytes(input, 4*SPX_N);
+    randombytes((unsigned char *)addr, 4 * 8 * sizeof(uint32_t));
+
+    printf("Testing if thash matches thashx4.. ");
+
+    for (j = 0; j < 4; j++) {
+        thash(out4 + j * SPX_N, input + j * SPX_N, 1, seed, addr + j*8);
+    }
+
+    thashx4(output + 0*SPX_N,
+            output + 1*SPX_N,
+            output + 2*SPX_N,
+            output + 3*SPX_N,
+            input + 0*SPX_N,
+            input + 1*SPX_N,
+            input + 2*SPX_N,
+            input + 3*SPX_N,
+            1, seed, addr);
+
+    if (memcmp(out4, output, 4 * SPX_N)) {
+        printf("failed!\n");
+        return -1;
+    }
+    printf("successful.\n");
+    return 0;
+}
