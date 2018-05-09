d13f12d0c82497d3320b1c5e78dd7772c91e658c
Joost Rijneveld <joost@joostrijneveld.nl>
2018-05-09 15:19:35 +0200

    Make benchmarks more elaborate

--- ref/test/benchmark.c
+++ ref/test/benchmark.c
@@ -3,10 +3,36 @@
 #include <time.h>
 
 #include "../api.h"
+#include "../fors.h"
+#include "../wots.h"
 #include "../params.h"
 #include "../randombytes.h"
 
 #define SPX_MLEN 32
+#define NTESTS 10
+
+static int cmp_llu(const void *a, const void*b)
+{
+  if(*(unsigned long long *)a < *(unsigned long long *)b) return -1;
+  if(*(unsigned long long *)a > *(unsigned long long *)b) return 1;
+  return 0;
+}
+
+static unsigned long long median(unsigned long long *l, size_t llen)
+{
+  qsort(l,llen,sizeof(unsigned long long),cmp_llu);
+
+  if(llen%2) return l[llen/2];
+  else return (l[llen/2-1]+l[llen/2])/2;
+}
+
+static void delta(unsigned long long *l, size_t llen)
+{
+    unsigned int i;
+    for(i = 0; i < llen - 1; i++) {
+        l[i] = l[i+1] - l[i];
+    }
+}
 
 static unsigned long long cpucycles(void)
 {
@@ -15,6 +41,59 @@ static unsigned long long cpucycles(void)
     : "=a" (result) ::  "%rdx");
   return result;
 }
+
+static void printfcomma (unsigned long long n)
+{
+    if (n < 1000) {
+        printf("%llu", n);
+        return;
+    }
+    printfcomma(n / 1000);
+    printf (",%03llu", n % 1000);
+}
+
+static void printfalignedcomma (unsigned long long n, int len)
+{
+    unsigned long long ncopy = n;
+    int i = 0;
+
+    while (ncopy > 9) {
+        len -= 1;
+        ncopy /= 10;
+        i += 1;  // to account for commas
+    }
+    i = i/3 - 1;  // to account for commas
+    for (; i < len; i++) {
+        printf(" ");
+    }
+    printfcomma(n);
+}
+
+static void display_result(double result, unsigned long long *l, size_t llen, unsigned long long mul)
+{
+    unsigned long long med;
+
+    result /= NTESTS;
+    delta(l, NTESTS + 1);
+    med = median(l, llen);
+    printf("avg. %10.2lf us (%1.2lf sec); median ", result, result / 1e6);
+    printfalignedcomma(med, 12);
+    printf(" cycles,  %5llux: ", mul);
+    printfalignedcomma(mul*med, 12);
+    printf(" cycles\n");
+}
+
+#define MEASURE(TEXT, MUL, FNCALL)\
+    printf(TEXT);\
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);\
+    for(i = 0; i < NTESTS; i++) {\
+        t[i] = cpucycles();\
+        FNCALL;\
+    }\
+    t[NTESTS] = cpucycles();\
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);\
+    result = (stop.tv_sec - start.tv_sec) * 1e6 + (stop.tv_nsec - start.tv_nsec) / 1e3;\
+    display_result(result, t, NTESTS, MUL);
 
 int main()
 {
@@ -26,46 +105,39 @@ int main()
     unsigned char *m = malloc(SPX_MLEN);
     unsigned char *sm = malloc(SPX_BYTES + SPX_MLEN);
     unsigned char *mout = malloc(SPX_BYTES + SPX_MLEN);
+
+    unsigned char fors_pk[SPX_FORS_PK_BYTES];
+    unsigned char fors_m[SPX_FORS_MSG_BYTES];
+    unsigned char fors_sig[SPX_FORS_BYTES];
+    unsigned char addr[SPX_ADDR_BYTES];
+
+    unsigned char wots_sig[SPX_WOTS_BYTES];
+    unsigned char wots_m[SPX_N];
+    unsigned char wots_pk[SPX_WOTS_PK_BYTES];
+
     unsigned long long smlen;
     unsigned long long mlen;
-    unsigned long long t0, t1;
+    unsigned long long t[NTESTS+1];
     struct timespec start, stop;
     double result;
+    int i;
 
     randombytes(m, SPX_MLEN);
+    randombytes(addr, SPX_ADDR_BYTES);
 
     printf("Parameters: n = %d, h = %d, d = %d, b = %d, k = %d, w = %d\n",
            SPX_N, SPX_FULL_HEIGHT, SPX_D, SPX_FORS_HEIGHT, SPX_FORS_TREES,
            SPX_WOTS_W);
 
-    printf("Generating keypair.. ");
+    printf("Running %d iterations.\n", NTESTS);
 
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    t0 = cpucycles();
-    crypto_sign_keypair(pk, sk);
-    t1 = cpucycles();
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
-    result = (stop.tv_sec - start.tv_sec) * 1e6 + (stop.tv_nsec - start.tv_nsec) / 1e3;
-    printf("took %lf us (%.2lf sec), %llu cycles\n", result, result / 1e6, t1 - t0);
-
-    printf("Signing.. ");
-
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    t0 = cpucycles();
-    crypto_sign(sm, &smlen, m, SPX_MLEN, sk);
-    t1 = cpucycles();
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
-    result = (stop.tv_sec - start.tv_sec) * 1e6 + (stop.tv_nsec - start.tv_nsec) / 1e3;
-    printf("took %lf us (%.2lf sec), %llu cycles\n", result, result / 1e6, t1 - t0);
-
-    printf("Verifying.. ");
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    t0 = cpucycles();
-    crypto_sign_open(mout, &mlen, sm, smlen, pk);
-    t1 = cpucycles();
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
-    result = (stop.tv_sec - start.tv_sec) * 1e6 + (stop.tv_nsec - start.tv_nsec) / 1e3;
-    printf("took %lf us (%.2lf sec), %llu cycles\n", result, result / 1e6, t1 - t0);
+    MEASURE("Generating keypair.. ", 1, crypto_sign_keypair(pk, sk));
+    MEASURE("  - WOTS pk gen..    ", (1 << SPX_TREE_HEIGHT), wots_gen_pk(wots_pk, sk, pk, (uint32_t *) addr));
+    MEASURE("Signing..            ", 1, crypto_sign(sm, &smlen, m, SPX_MLEN, sk));
+    MEASURE("  - FORS signing..   ", 1, fors_sign(fors_sig, fors_pk, fors_m, sk, pk, (uint32_t *) addr));
+    MEASURE("  - WOTS signing..   ", SPX_D, wots_sign(wots_sig, wots_m, sk, pk, (uint32_t *) addr));
+    MEASURE("  - WOTS pk gen..    ", SPX_D * (1 << SPX_TREE_HEIGHT), wots_gen_pk(wots_pk, sk, pk, (uint32_t *) addr));
+    MEASURE("Verifying..          ", 1, crypto_sign_open(mout, &mlen, sm, smlen, pk));
 
     printf("Signature size: %d (%.2f KiB)\n", SPX_BYTES, SPX_BYTES / 1024.0);
     printf("Public key size: %d (%.2f KiB)\n", SPX_PK_BYTES, SPX_PK_BYTES / 1024.0);
