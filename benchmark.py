8ab8ab1794b5d48fdebe3e06e54bfb65446bd261
Joost Rijneveld <joost@joostrijneveld.nl>
2019-03-12 16:21:23 +0100

    Update benchmark script to check thash variants

--- benchmark.py
+++ benchmark.py
@@ -9,44 +9,38 @@ implementations = [
                    ('ref', ['shake256', 'sha256', 'haraka']),
                    ('haraka-aesni', ['haraka']),
                    ('shake256-avx2', ['shake256']),
+                   ('sha256-avx2', ['sha256']),
                    ]
 
 options = ["f", "s"]
 sizes = [128, 192, 256]
+thashes = ['robust', 'simple']
 
 PARAMDIR = "../ref/params/"  # relative to an implementation directory
 
 try:
     for impl, fns in implementations:
         params = os.path.join(impl, "params.h")
-        makefile = os.path.join(impl, "Makefile")
         run(["mv", params, params+'.keep'], stdout=DEVNULL, stderr=DEVNULL)
-        run(["cp", makefile, makefile+'.keep'], stdout=DEVNULL, stderr=DEVNULL)
         for fn in fns:
-            with fileinput.FileInput(makefile, inplace=True) as f:
-                for line in f:
-                    if line.startswith("HASH_C = hash_"):
-                        print("HASH_C = hash_{}.c".format(fn))
-                    else:
-                        print(line, end='')
-            for opt, size in itertools.product(options, sizes):
+            for opt, size, thash in itertools.product(options, sizes, thashes):
                 paramset = "sphincs-{}-{}{}".format(fn, size, opt)
                 paramfile = "params-{}.h".format(paramset)
-                print("## Benchmarking", paramset, "using", impl)
+                print("Benchmarking", paramset, thash, "using", impl)
+                hashf = 'HASH={}'.format(fn)  # overrides Makefile var
+                thash = 'THASH={}'.format(thash)  # overrides Makefile var
                 run(["ln", "-fs", os.path.join(PARAMDIR, paramfile), params],
-                    stdout=DEVNULL, stderr=DEVNULL)
-                run(["make", "-C", impl, "clean"],
-                    stdout=DEVNULL, stderr=DEVNULL)
-                run(["make", "-C", impl, "benchmarks"],
-                    stdout=DEVNULL, stderr=DEVNULL)
-                run(["make", "-C", impl, "benchmark"],
-                    stdout=sys.stdout, stderr=DEVNULL)
+                    stdout=DEVNULL, stderr=sys.stderr)
+                run(["make", "-C", impl, "clean", thash, hashf],
+                    stdout=DEVNULL, stderr=sys.stderr)
+                run(["make", "-C", impl, "benchmarks", thash, hashf],
+                    stdout=DEVNULL, stderr=sys.stderr)
+                run(["make", "-C", impl, "benchmark", thash, hashf],
+                    stdout=sys.stdout, stderr=sys.stderr)
                 print()
 
 finally:
     print("Cleaning up..")
     for impl, fns in implementations:
         params = os.path.join(impl, "params.h")
-        makefile = os.path.join(impl, "Makefile")
         run(["mv", params+'.keep', params], stdout=DEVNULL, stderr=DEVNULL)
-        run(["mv", makefile+'.keep', makefile], stdout=DEVNULL, stderr=DEVNULL)
