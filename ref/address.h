6c18272c559b33b678ec1857e028345e764a6485
sfluhrer <sfluhrer@cisco.com>
2020-07-04 23:10:14 -0400

    Eliminate addr_to_bytes

--- ref/address.h
+++ ref/address.h
@@ -9,31 +9,29 @@
 #define SPX_ADDR_TYPE_FORSTREE 3
 #define SPX_ADDR_TYPE_FORSPK 4
 
-void addr_to_bytes(unsigned char *bytes, const uint32_t addr[8]);
+void set_layer_addr(uint8_t addr[32], uint32_t layer);
 
-void set_layer_addr(uint32_t addr[8], uint32_t layer);
+void set_tree_addr(uint8_t addr[32], uint64_t tree);
 
-void set_tree_addr(uint32_t addr[8], uint64_t tree);
-
-void set_type(uint32_t addr[8], uint32_t type);
+void set_type(uint8_t addr[32], uint32_t type);
 
 /* Copies the layer and tree part of one address into the other */
-void copy_subtree_addr(uint32_t out[8], const uint32_t in[8]);
+void copy_subtree_addr(uint8_t out[32], const uint8_t in[32]);
 
 /* These functions are used for WOTS and FORS addresses. */
 
-void set_keypair_addr(uint32_t addr[8], uint32_t keypair);
+void set_keypair_addr(uint8_t addr[32], uint32_t keypair);
 
-void set_chain_addr(uint32_t addr[8], uint32_t chain);
+void set_chain_addr(uint8_t addr[32], uint32_t chain);
 
-void set_hash_addr(uint32_t addr[8], uint32_t hash);
+void set_hash_addr(uint8_t addr[32], uint32_t hash);
 
-void copy_keypair_addr(uint32_t out[8], const uint32_t in[8]);
+void copy_keypair_addr(uint8_t out[32], const uint8_t in[32]);
 
 /* These functions are used for all hash tree addresses (including FORS). */
 
-void set_tree_height(uint32_t addr[8], uint32_t tree_height);
+void set_tree_height(uint8_t addr[32], uint32_t tree_height);
 
-void set_tree_index(uint32_t addr[8], uint32_t tree_index);
+void set_tree_index(uint8_t addr[21], uint32_t tree_index);
 
 #endif
