494e1dc9e4aaccedc30a14bbd42163b99ad2a8c9
Bas Westerbaan <bas@cloudflare.com>
2022-06-01 12:48:27 +0200

    thashx8: add two block test

--- sha256-avx2/test/thashx8.c
+++ sha256-avx2/test/thashx8.c
@@ -12,7 +12,7 @@ int main()
     /* Make stdout buffer more responsive. */
     setbuf(stdout, NULL);
 
-    unsigned char input[8*SPX_N];
+    unsigned char input[16*SPX_N];
     spx_ctx ctx;
     unsigned char output[8*SPX_N];
     unsigned char out8[8*SPX_N];
@@ -20,12 +20,12 @@ int main()
     unsigned int j;
 
     randombytes(ctx.pub_seed, SPX_N);
-    randombytes(input, 8*SPX_N);
+    randombytes(input, 16*SPX_N);
     randombytes((unsigned char *)addr, 8 * 8 * sizeof(uint32_t));
 
     initialize_hash_function(&ctx);
 
-    printf("Testing if thash matches thashx8.. ");
+    printf("Testing if thash matches thashx8 on one block ... ");
 
     for (j = 0; j < 8; j++) {
         thash(out8 + j * SPX_N, input + j * SPX_N, 1, &ctx, addr + j*8);
@@ -51,6 +51,36 @@ int main()
 
     if (memcmp(out8, output, 8 * SPX_N)) {
         printf("failed!\n");
+        return -1;
+    }
+    printf("successful.\n");
+
+    printf("Testing if thash matches thashx8 on two blocks ... ");
+
+    for (j = 0; j < 8; j++) {
+        thash(out8 + j * SPX_N, input + (2*j) * SPX_N, 2, &ctx, addr + j*8);
+    }
+
+    thashx8(output + 0*SPX_N,
+            output + 1*SPX_N,
+            output + 2*SPX_N,
+            output + 3*SPX_N,
+            output + 4*SPX_N,
+            output + 5*SPX_N,
+            output + 6*SPX_N,
+            output + 7*SPX_N,
+            input + 0*SPX_N,
+            input + 2*SPX_N,
+            input + 4*SPX_N,
+            input + 6*SPX_N,
+            input + 8*SPX_N,
+            input + 10*SPX_N,
+            input + 12*SPX_N,
+            input + 14*SPX_N,
+            2, &ctx, addr);
+
+    if (memcmp(out8, output, 8 * SPX_N)) {
+        printf("\nfailed!\n");
         return -1;
     }
     printf("successful.\n");
