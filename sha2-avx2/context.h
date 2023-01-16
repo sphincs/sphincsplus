c5054bdbd238a14bc856b471acc79b5605e7770c
Thom Wiggers <thom@thomwiggers.nl>
2023-01-16 14:14:35 +0100

    Better align context.h
    clang-tidy suggested that this layout was more efficient.

--- sha2-avx2/context.h
+++ sha2-avx2/context.h
@@ -9,15 +9,18 @@
 #include "sha512x4.h"
 
 typedef struct {
-    uint8_t pub_seed[SPX_N];
-    uint8_t sk_seed[SPX_N];
-
-    sha256ctx state_seeded;
     sha256x8ctx statex8_seeded;
 #if SPX_SHA512
-    sha512ctx state_seeded_512;
     sha512x4ctx statex4_seeded_512;
 #endif
+
+    sha256ctx state_seeded;
+#if SPX_SHA512
+    sha512ctx state_seeded_512;
+#endif
+
+    uint8_t pub_seed[SPX_N];
+    uint8_t sk_seed[SPX_N];
 } spx_ctx;
 
 
